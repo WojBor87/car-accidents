c3558f486ee0e397246a8c135599bbe07a2c29d6
Wojciech Borawski <w.borawski@ihar.edu.pl>
2021-03-06 09:39:37 +0100

    Add fields to tables in models.py file

--- car_accidents/frontend/models.py
+++ car_accidents/frontend/models.py
@@ -2,53 +2,68 @@ from django.db import models
 
 
 # Create your models here.
-class Accident(models.Model):
-    pass
-
-
-class District(models.Model):
-    pass
-
-
-class DistrictCommune(models.Model):
-    pass
-
-
-class CharPlaceOfTheEvent(models.Model):
-    pass
-
-
-class WeatherConditions(models.Model):
-    pass
-
-
-class Lighting(models.Model):
-    pass
+class RoadCategory(models.Model):
+    name = models.CharField(max_length=30)
 
 
 class Road(models.Model):
-    pass
+    name = models.CharField(max_length=15)
+    road_category_id = models.ForeignKey(RoadCategory, on_delete=models.CASCADE)
 
 
-class RoadCategory(models.Model):
-    pass
+class Voivodeship(models.Model):
+    name = models.CharField(max_length=20)
 
 
-class TypeOfRoad(models.Model):
-    pass
+class District(models.Model):
+    name = models.CharField(max_length=15)
+    voivodeship_id = models.ForeignKey(Voivodeship, on_delete=models.CASCADE)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=35)
+    district_id = models.ForeignKey(District, on_delete=models.CASCADE)
+    is_city = models.SmallIntegerField()
 
 
 class RoadGeometry(models.Model):
-    pass
+    name = models.IntegerField()
 
 
-class Area(models.Model):
-    pass
-
-
-class TypeOfAccident(models.Model):
-    pass
+class PlaceOfTheEvent(models.Model):
+    name = models.CharField(max_length=70)
 
 
 class TypeOfInjury(models.Model):
-    pass
+    name = models.CharField(max_length=40)
+
+
+class TypeOfAccident(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Lighting(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class WeatherConditions(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Accident(models.Model):
+    data_time = models.DateTimeField()
+    location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    road_id = models.ForeignKey(Road, on_delete=models.CASCADE)
+    is_built_up_area = models.SmallIntegerField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    road_geometry_id = models.ForeignKey(RoadGeometry, on_delete=models.CASCADE)
+    place_of_the_event_id = models.ForeignKey(PlaceOfTheEvent, on_delete=models.CASCADE)
+    weather_conditions_id = models.ForeignKey(WeatherConditions, on_delete=models.CASCADE)
+    lighting_id = models.ForeignKey(Lighting, on_delete=models.CASCADE)
+    type_of_accident_id = models.ForeignKey(TypeOfAccident, on_delete=models.CASCADE)
+    num_of_accidents = models.IntegerField()
+    num_of_fatalities = models.IntegerField()
+    num_of_injured = models.IntegerField()
+    type_of_injury_id = models.ForeignKey(TypeOfInjury, on_delete=models.CASCADE)
+    is_offender_intoxicated = models.SmallIntegerField()
