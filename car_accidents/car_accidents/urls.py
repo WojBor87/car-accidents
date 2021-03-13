3e5779b551589e5fb61722e5ad9bf78b19b3f1a0
krisu-g <grabowski.krzysztof@gmail.com>
2021-03-13 13:36:21 +0100

    feature_accident_filter_view

--- car_accidents/car_accidents/urls.py
+++ car_accidents/car_accidents/urls.py
@@ -9,8 +9,7 @@ urlpatterns = (
     + favicon_patterns
     + [
         path('', include('frontend.urls')),
-    path('login/', auth_views.LoginView.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-
+        path('login/', auth_views.LoginView.as_view(), name="login"),
+        path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     ]
 )
