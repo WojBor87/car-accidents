a50079b0a5797c70edcc88524a1dadf303a9debb
Wojciech Borawski <w.borawski@ihar.edu.pl>
2021-03-13 10:36:39 +0100

    Fix filter.py - all filters add Install django-widget-tweaks, and add it to requirements.txt

--- car_accidents/frontend/filters.py
+++ car_accidents/frontend/filters.py
@@ -5,4 +5,21 @@ import django_filters
 class AccidentFilter(django_filters.FilterSet):
     class Meta:
         model = Accident
-        fields =['idksip']
+        fields =[
+            'driver_behavior',
+            'lighting',
+            'pedestrian_behavior',
+            'place_of_the_event',
+            'road',
+            'road_geometry',
+            'town_name',
+            'type_of_accident',
+            'type_of_injury',
+            'weather_conditions',
+            "is_built_up_area",
+            "data_time",
+            "num_of_accidents",
+            "num_of_fatalities",
+            "num_of_injured",
+            "is_offender_intoxicated",
+        ]
