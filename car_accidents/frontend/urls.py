a52c533362c8f003994b3f4650dde6bdfa7a2b27
Wojciech Borawski <w.borawski@ihar.edu.pl>
2021-03-14 09:06:10 +0100

    Cleaning #1

--- car_accidents/frontend/urls.py
+++ car_accidents/frontend/urls.py
@@ -6,9 +6,8 @@ from . import views
 app_name = 'frontend'
 
 urlpatterns = [
-    path('', views.filters_form, name='filters_form'),
+    path('', views.home, name='home'),
     path('helper/', include('frontend.helper_urls')),
     path('import_csv/<file_name>', views.ImportCsvView.as_view(), name='import_csv'),
-    path('accident_view/', views.AccidentView.as_view(), name='accident_view'),
     path('accident_filter_view/', views.filter_search, name='filter')
 ]
